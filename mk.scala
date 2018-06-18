e0f3373c1a43f21bb5190c4872debf2f3c01c162
Nada Amin <na482@cl.cam.ac.uk>
2018-06-18 18:54:35 +0100

    a touch of types

--- mk.scala
+++ mk.scala
@@ -2,14 +2,14 @@
 // https://github.com/TheReasonedSchemer2ndEd/CodeFromTheReasonedSchemer2ndEd/blob/master/trs2-impl.scm
 
 trait MicroKanren {
-  class Term
-  class Var(x: String) extends Term // no case class for reference equality!
-  case class Atom(s: String) extends Term
-  case class Fun(s: String, vs: List[Term]) extends Term
-  type Subst = Map[Var,Term]
-  def walk(v: Term, s: Subst): Term = {
+  class Term[+T]
+  class Var[+T](x: String) extends Term[T] // no case class for reference equality!
+  case class Atom[+T](s: String) extends Term[T]
+  case class Fun[+T](s: String, vs: List[Term[Any]]) extends Term[T]
+  type Subst = Map[Var[Any],Term[Any]]
+  def walk(v: Term[Any], s: Subst): Term[Any] = {
     v match {
-      case v:Var => s.get(v) match {
+      case v:Var[Any] => s.get(v) match {
         case Some(v) => walk(v, s)
         case None => v
       }
@@ -23,32 +23,32 @@ trait MicroKanren {
   case class IThunk(f: () => Inf) extends Inf
   type Goal = Subst => Inf
 
-  def ext_s(x: Var, v: Term, s: Subst) = {
+  def ext_s(x: Var[Any], v: Term[Any], s: Subst) = {
     if (occurs(x, v, s)) None
     else Some(s + (x -> v))
   }
 
-  def occurs(x: Var, v: Term, s: Subst): Boolean = {
+  def occurs(x: Var[Any], v: Term[Any], s: Subst): Boolean = {
     walk(v, s) match {
-      case v:Var => v == x
+      case v:Var[Any] => v == x
       case Fun(_, vs) =>
         vs.exists{v => occurs(x, v, s)}
       case v => false
     }
   }
 
-  def unify(u: Term, v: Term, s: Subst): Option[Subst] = {
+  def unify(u: Term[Any], v: Term[Any], s: Subst): Option[Subst] = {
     (walk(u, s), walk(v, s)) match {
       case (u, v) if u==v => Some(s)
-      case (u:Var,v) => ext_s(u, v, s)
-      case (u,v:Var) => ext_s(v, u, s)
+      case (u:Var[Any],v) => ext_s(u, v, s)
+      case (u,v:Var[Any]) => ext_s(v, u, s)
       case (Fun(ut, us), Fun(vt, vs)) if ut==vt =>
         unify_ls(us, vs, s)
       case _ => None
     }
   }
 
-  def unify_ls(us: List[Term], vs: List[Term], s: Subst): Option[Subst] = {
+  def unify_ls(us: List[Term[Any]], vs: List[Term[Any]], s: Subst): Option[Subst] = {
     (us, vs) match {
       case (Nil, Nil) => Some(s)
       case (u::us, v::vs) =>
@@ -59,14 +59,14 @@ trait MicroKanren {
     }
   }
 
-  def infix_===(u: Term, v: Term): Goal = { s =>
+  def infix_===[T](u: Term[T], v: Term[T]): Goal = { s =>
     unify(u, v, s) match {
       case Some(s) => ICons(s, INil)
       case None => INil
     }
   }
-  implicit class TermEq(u: Term) {
-    def ===(v: Term) = infix_===(u, v)
+  implicit class TermEq[T](u: Term[T]) {
+    def ===(v: Term[T]) = infix_===(u, v)
   }
 
   def succeed: Goal = { s => ICons(s, INil) }
@@ -117,23 +117,23 @@ trait MicroKanren {
     }
   }
 
-  def call_fresh(name: String, f: Var => Goal) =
+  def call_fresh[T](name: String, f: Var[T] => Goal) =
     f(new Var(name))
 
   def reify_name(n: Int) = "_" + n.toString
 
-  def walk_star(v: Term, s: Subst): Term = {
+  def walk_star(v: Term[Any], s: Subst): Term[Any] = {
     walk(v, s) match {
-      case v:Var => v
+      case v:Var[Any] => v
       case Fun(t, vs) =>
         Fun(t, vs.map{v => walk_star(v, s)})
       case v => v
     }
   }
 
-  def reify_s(v: Term, r: Subst) = {
+  def reify_s(v: Term[Any], r: Subst) = {
     walk(v, r) match {
-      case v:Var =>
+      case v:Var[Any] =>
         val n = r.size
         val rn = reify_name(n)
         r + (v -> Atom(rn))
@@ -143,7 +143,7 @@ trait MicroKanren {
     }
   }
 
-  def reify_ls(vs: List[Term], r: Subst): Subst = {
+  def reify_ls(vs: List[Term[Any]], r: Subst): Subst = {
     vs match {
       case Nil => r
       case (v::vs) =>
@@ -151,7 +151,7 @@ trait MicroKanren {
     }
   }
 
-  def reify(v: Term) = { s: Subst =>
+  def reify(v: Term[Any]) = { s: Subst =>
     val v2 = walk_star(v, s)
     val r = reify_s(v2, Map.empty)
     walk_star(v2, r)
@@ -184,26 +184,27 @@ trait MicroKanren {
     IThunk{ () => g(s) }
   }
 
-  def run(n: Option[Int])(f: Var => Goal) = {
+  def run[T](n: Option[Int])(f: Var[T] => Goal) = {
     val q = new Var("q")
     run_goal(n, f(q)).map(reify(q))
   }
-  def runA(f: Var => Goal) = run(None)(f)
-  def runN(n: Int)(f: Var => Goal) = run(Some(n))(f)
-  def run1(f: Var => Goal) = runN(1)(f)
+  def runA[T](f: Var[T] => Goal) = run(None)(f)
+  def runN[T](n: Int)(f: Var[T] => Goal) = run(Some(n))(f)
+  def run1[T](f: Var[T] => Goal) = runN(1)(f)
 
   def nil = Atom("nil")
-  def cons(a: Term, b: Term) = Fun("cons", List(a,b))
-  def lift(xs: List[Term]): Term = xs match {
+  def cons[T](v: Term[T], vs: Term[List[T]]) = Fun[List[T]]("cons", List(v,vs))
+  def pair[A,B](a: Term[A], b: Term[B]) = Fun[(A,B)]("pair", List(a, b))
+  def lift[T](xs: List[Term[T]]): Term[List[T]] = xs match {
     case Nil => nil
     case x::xs => cons(x, lift(xs))
   }
-  def fresh(f: Var => Goal) = call_fresh("x", f)
-  def freshN(n: Int)(f: List[Var] => Goal) =
+  def fresh[T](f: Var[T] => Goal) = call_fresh("x", f)
+  def freshN[T](n: Int)(f: List[Var[T]] => Goal) =
     f((0 until n).map{_ => new Var("x")}.toList)
 
-  def lst(xs: Term*) = lift(xs.toList)
-  def i(n: Int) = Atom(n.toString)
+  def lst[T](xs: Term[T]*) = lift(xs.toList)
+  def i(n: Int) = Atom[Int](n.toString)
 }
 
 object mk extends MicroKanren
@@ -211,32 +212,32 @@ object mk extends MicroKanren
 object tests extends App {
   import mk._
 
-  assert(run1{q => succeed} == List(Atom("_0")))
-  assert(runA{q => succeed} == List(Atom("_0")))
+  assert(run1[Any]{q => succeed} == List(Atom("_0")))
+  assert(runA[Any]{q => succeed} == List(Atom("_0")))
 
-  assert(run1{q => freshN(2){ case List(x, y) => q === cons(x,y) }} == List(Fun("cons",List(Atom("_0"), Atom("_1")))))
-  assert(runA{q => fresh{x => q === cons(x, x)}} == List(Fun("cons",List(Atom("_0"), Atom("_0")))))
-  assert(runA{q => fresh{x => fresh{y => q === cons(x, y)}}} == List(Fun("cons",List(Atom("_0"), Atom("_1")))))
-  assert(runA{q => freshN(2){case List(x, y) => q === cons(x,y)}} == List(Fun("cons",List(Atom("_0"), Atom("_1")))))
+  assert(run1[Any]{q => freshN(2){ case List(x, y) => q === pair(x,y) }} == List(Fun("pair",List(Atom("_0"), Atom("_1")))))
+  assert(runA[Any]{q => fresh[Any]{x => q === pair(x, x)}} == List(Fun("pair",List(Atom("_0"), Atom("_0")))))
+  assert(runA[Any]{q => fresh[Any]{x => fresh[Any]{y => q === pair(x, y)}}} == List(Fun("pair",List(Atom("_0"), Atom("_1")))))
+  assert(runA[Any]{q => freshN[Any](2){case List(x, y) => q === pair(x,y)}} == List(Fun("pair",List(Atom("_0"), Atom("_1")))))
 
-  assert(runA{q => succeed || fail} == List(Atom("_0")))
+  assert(runA[Any]{q => succeed || fail} == List(Atom("_0")))
   def loopo(): Goal = rel{fail || loopo()}
-  assert(run1{q => succeed || loopo()} == List(Atom("_0")))
+  assert(run1[Any]{q => succeed || loopo()} == List(Atom("_0")))
 
-  assert(run1{q => q === i(1) || q === i(2)} == List(Atom("1")))
-  assert(runA{q => q === i(1) || q === i(2)} == List(Atom("1"), Atom("2")))
+  assert(run1[Any]{q => q === i(1) || q === i(2)} == List(Atom("1")))
+  assert(runA[Any]{q => q === i(1) || q === i(2)} == List(Atom("1"), Atom("2")))
 
-  def appendo(xs: Term, ys: Term, zs: Term): Goal = rel{
+  def appendo[T](xs: Term[List[T]], ys: Term[List[T]], zs: Term[List[T]]): Goal = rel{
     (xs === nil && ys === zs) ||
-    freshN(3){ case List(first, rest, rs) =>
+    fresh[T]{ first => freshN[List[T]](2){ case List(rest, rs) =>
       cons(first, rest) === xs &&
       cons(first, rs) === zs &&
       appendo(rest, ys, rs)
-    }
+    }}
   }
 
-  assert(runA{q => appendo(lst(i(1),i(2)), lst(i(3),i(4),i(5)), q)} == List(lst(i(1),i(2),i(3),i(4),i(5))))
-  assert(runA{q => freshN(2){ case List(x,y) => q === cons(x,y) && appendo(x, y, lst(i(1),i(2),i(3),i(4),i(5))) }} == List(
+  assert(runA[List[Int]]{q => appendo(lst(i(1),i(2)), lst(i(3),i(4),i(5)), q)} == List(lst(i(1),i(2),i(3),i(4),i(5))))
+  assert(runA[Any]{q => freshN[List[Int]](2){ case List(x,y) => q === cons(x,y) && appendo(x, y, lst(i(1),i(2),i(3),i(4),i(5))) }} == List(
     cons(nil, lst(i(1),i(2),i(3),i(4),i(5))),
     cons(lst(i(1)), lst(i(2),i(3),i(4),i(5))),
     cons(lst(i(1),i(2)), lst(i(3),i(4),i(5))),

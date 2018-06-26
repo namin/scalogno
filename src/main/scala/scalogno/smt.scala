d37267aba1a0b5e16935ce80d16d48725dca1106
Nada Amin <na482@cl.cam.ac.uk>
2018-06-26 18:07:08 +0100

    exe process

--- src/main/scala/scalogno/smt.scala
+++ src/main/scala/scalogno/smt.scala
@@ -1,8 +1,35 @@
 package scalogno
 
-trait SmtShell {
+class Exe(command: String) {
 
+  import scala.sys.process._
+  import scala.io._
+  import java.io._
+  import scala.concurrent._
+
+  val inputStream = new SyncVar[OutputStream]
+  val outputStream = new SyncVar[BufferedReader]
+
+  val process = Process(command).run(
+    new ProcessIO(
+      stdin => inputStream.put(stdin),
+      stdout => outputStream.put(new BufferedReader(new InputStreamReader(stdout))),
+      stderr => Source.fromInputStream(stderr).getLines.foreach(println)));
+
+  def readLine(): String = synchronized {
+    outputStream.get.readLine()
+  }
+
+  def write(s: String): Unit = synchronized {
+    inputStream.get.write((s + "\n").getBytes)
+  }
+
+  def close(): Unit = {
+    inputStream.get.close
+    outputStream.get.close
+  }
 }
 
+trait SmtShell
 object Play extends SmtShell
 

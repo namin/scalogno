e5017141c82c9f7fb6f76524abd0e267cd67b66a
Nada Amin <na482@cl.cam.ac.uk>
2018-06-26 06:59:41 +0100

    work around dvars

--- src/main/scala/scalogno/engine.scala
+++ src/main/scala/scalogno/engine.scala
@@ -129,21 +129,32 @@ def register(c: Constraint): Unit = {
   if (cstore.contains(c)) return
   if (conflict(cstore,c)) throw Backtrack
 }
+
+type Thread = immutable.Map[String,Any]
+var thread: Thread = Map.empty
+def thread_save() = {
+  thread = Map.empty
+  thread += ("cstore" -> cstore)
+  thread += ("dvars" -> dvars)
+  thread
+}
+def thread_restore(thread: Thread) = {
+  cstore = thread("cstore").asInstanceOf[immutable.Set[Constraint]]
+  dvars = thread("dvars").asInstanceOf[immutable.Map[Int, Any]]
+}
 }
 
 trait Engine extends Base {
 // execution (depth-first)
 def run[T](f: Exp[T] => Rel): Seq[String] = {
   def call(e: () => Rel)(k: Cont): Unit = {
-    val cstore1 = cstore
-    val dvars1 = dvars
+    val thread = thread_save()
     try {
       e().exec(call)(k)
     } catch {
       case Backtrack => // OK
     } finally {
-      cstore = cstore1
-      dvars = dvars1
+      thread_restore(thread)
     }
   }
   val q = fresh[T]

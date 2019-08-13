74da341e44973599cce09610e79fbee5d0ff8a6b
Nada Amin <namin@alum.mit.edu>
2019-08-13 10:42:32 -0400

    indent

--- src/main/scala/scalogno/engine.scala
+++ src/main/scala/scalogno/engine.scala
@@ -3,14 +3,14 @@ package scalogno
 import scala.collection._
 
 trait Base {
-val Backtrack = new Exception
+  val Backtrack = new Exception
 
-var varCount: Int = 0
-def freshId = {
-  var id = varCount
-  varCount += 1
-  id
-}
+  var varCount: Int = 0
+  def freshId = {
+    var id = varCount
+    varCount += 1
+    id
+  }
 
   implicit class RelOps(a: => Rel) {
     def &&(b: => Rel) = infix_&&(a,b)
@@ -39,145 +39,145 @@ def freshId = {
     f(fresh[T],fresh[U],fresh[V],fresh[W],fresh[X],fresh[Y],fresh[Z])
   }
 
-// dynamically scoped variables
-var dvars: immutable.Map[Int, Any] = immutable.Map.empty
-case class DVar[T](val id: Int, val default: T) extends (() => T) {
-  dvar_set(id,default)
-  def apply()  = dvar_get[T](id)
-  def :=(v: T) = dvar_set[T](id,v)
-}
-var dvarCount = 1
-def DVar[T](v: T): DVar[T] = {
-  val id = dvarCount
-  dvarCount += 1
-  new DVar[T](id, v)
-}
-def dvar_set[T](id: Int, v: T): Unit = dvars += id -> v
-def dvar_get[T](id: Int): T = dvars(id).asInstanceOf[T]
-def dvar_upd[T](id: Int)(f: T => T): Unit = dvars += id -> f(dvar_get(id))
+  // dynamically scoped variables
+  var dvars: immutable.Map[Int, Any] = immutable.Map.empty
+  case class DVar[T](val id: Int, val default: T) extends (() => T) {
+    dvar_set(id,default)
+    def apply()  = dvar_get[T](id)
+    def :=(v: T) = dvar_set[T](id,v)
+  }
+  var dvarCount = 1
+  def DVar[T](v: T): DVar[T] = {
+    val id = dvarCount
+    dvarCount += 1
+    new DVar[T](id, v)
+  }
+  def dvar_set[T](id: Int, v: T): Unit = dvars += id -> v
+  def dvar_get[T](id: Int): T = dvars(id).asInstanceOf[T]
+  def dvar_upd[T](id: Int)(f: T => T): Unit = dvars += id -> f(dvar_get(id))
 
-// relations
-trait Rel { def exec(call: Exec)(k: Cont): Unit }
-type Exec = (() => Rel) => Cont => Unit
-type Cont = () => Unit
+  // relations
+  trait Rel { def exec(call: Exec)(k: Cont): Unit }
+  type Exec = (() => Rel) => Cont => Unit
+  type Cont = () => Unit
 
-val Yes = new Rel { 
-  def exec(call: Exec)(k: Cont) = k() }
+  val Yes = new Rel {
+    def exec(call: Exec)(k: Cont) = k() }
 
-val No = new Rel { 
-  def exec(call: Exec)(k: Cont) = throw Backtrack }
+  val No = new Rel {
+    def exec(call: Exec)(k: Cont) = throw Backtrack }
 
-def infix_&&(a: => Rel, b: => Rel): Rel = new Rel {
-  def exec(call: Exec)(k: Cont) =
-    call(() => a) { () => call(() => b)(k) } }
+  def infix_&&(a: => Rel, b: => Rel): Rel = new Rel {
+    def exec(call: Exec)(k: Cont) =
+      call(() => a) { () => call(() => b)(k) } }
 
-def infix_||(a: => Rel, b: => Rel): Rel = new Rel {
-  def exec(call: Exec)(k: Cont) = {
-    call(() => a)(k); call(() => b)(k) }}
+  def infix_||(a: => Rel, b: => Rel): Rel = new Rel {
+    def exec(call: Exec)(k: Cont) = {
+      call(() => a)(k); call(() => b)(k) }}
 
-// logic terms
-case class Exp[+T](id: Int)
-def fresh[T] = Exp(freshId)
+  // logic terms
+  case class Exp[+T](id: Int)
+  def fresh[T] = Exp(freshId)
 
-def exists[T](f: Exp[T] => Rel): Rel = f(fresh)
+  def exists[T](f: Exp[T] => Rel): Rel = f(fresh)
 
-def infix_===[T](a: => Exp[T], b: => Exp[T]): Rel = {
-  register(IsEqual(a,b)); Yes }
+  def infix_===[T](a: => Exp[T], b: => Exp[T]): Rel = {
+    register(IsEqual(a,b)); Yes }
 
-def term[T](key: String, args: List[Exp[Any]]): Exp[T] = {
-  val e = fresh[T]; register(IsTerm(e.id, key, args)); e }  
+  def term[T](key: String, args: List[Exp[Any]]): Exp[T] = {
+    val e = fresh[T]; register(IsTerm(e.id, key, args)); e }
 
-// constraints
-abstract class Constraint
-case class IsTerm(id: Int, key: String, args: List[Exp[Any]]) 
-  extends Constraint
-case class IsEqual(x: Exp[Any], y: Exp[Any]) 
-  extends Constraint
+  // constraints
+  abstract class Constraint
+  case class IsTerm(id: Int, key: String, args: List[Exp[Any]])
+      extends Constraint
+  case class IsEqual(x: Exp[Any], y: Exp[Any])
+      extends Constraint
 
-var cstore: immutable.Set[Constraint] = immutable.Set.empty
-def conflict(cs: Set[Constraint], c: Constraint): Boolean = {
-  def prop(c1: Constraint, c2: Constraint)(fail: () => Nothing): List[Constraint] = (c1,c2) match {
-    case (IsEqual(a1,b1), IsEqual(a2,b2)) if a1 == a2 || a1 == b2 || b1 == a2 || b1 == b2 =>
-      List(IsEqual(a1,a2),IsEqual(a1,b2),IsEqual(b1,a2),IsEqual(b1,b2))
+  var cstore: immutable.Set[Constraint] = immutable.Set.empty
+  def conflict(cs: Set[Constraint], c: Constraint): Boolean = {
+    def prop(c1: Constraint, c2: Constraint)(fail: () => Nothing): List[Constraint] = (c1,c2) match {
+      case (IsEqual(a1,b1), IsEqual(a2,b2)) if a1 == a2 || a1 == b2 || b1 == a2 || b1 == b2 =>
+        List(IsEqual(a1,a2),IsEqual(a1,b2),IsEqual(b1,a2),IsEqual(b1,b2))
 
-    case (IsEqual(Exp(a),Exp(b)), IsTerm(a1, key, args)) if a == a1 =>
-      List(IsTerm(b, key, args))
-    case (IsTerm(a1, key, args), IsEqual(Exp(a),Exp(b))) if a == a1 =>
-      List(IsTerm(b, key, args))
-    case (IsEqual(Exp(a),Exp(b)), IsTerm(a1, key, args)) if a == a1 =>
-      List(IsTerm(b, key, args))
-    case (IsTerm(b1, key, args), IsEqual(Exp(a),Exp(b))) if b == b1 =>
-      List(IsTerm(a, key, args))
-    case (IsEqual(Exp(a),Exp(b)), IsTerm(b1, key, args)) if b == b1 =>
-      List(IsTerm(a, key, args))
+      case (IsEqual(Exp(a),Exp(b)), IsTerm(a1, key, args)) if a == a1 =>
+        List(IsTerm(b, key, args))
+      case (IsTerm(a1, key, args), IsEqual(Exp(a),Exp(b))) if a == a1 =>
+        List(IsTerm(b, key, args))
+      case (IsEqual(Exp(a),Exp(b)), IsTerm(a1, key, args)) if a == a1 =>
+        List(IsTerm(b, key, args))
+      case (IsTerm(b1, key, args), IsEqual(Exp(a),Exp(b))) if b == b1 =>
+        List(IsTerm(a, key, args))
+      case (IsEqual(Exp(a),Exp(b)), IsTerm(b1, key, args)) if b == b1 =>
+        List(IsTerm(a, key, args))
 
-    case (IsTerm(a1, key1, args1), IsTerm(a2, key2, args2)) if a1 == a2 =>
-      if (key1 != key2 || args1.length != args2.length) fail()
-      (args1,args2).zipped map (IsEqual(_,_))
-    case _ => Nil
+      case (IsTerm(a1, key1, args1), IsTerm(a2, key2, args2)) if a1 == a2 =>
+        if (key1 != key2 || args1.length != args2.length) fail()
+          (args1,args2).zipped map (IsEqual(_,_))
+      case _ => Nil
+    }
+
+    val fail = () => throw Backtrack
+
+    val cn = cs flatMap { c2 => prop(c, c2)(fail) }
+    cstore += c
+    cn foreach register
+    false
   }
 
-  val fail = () => throw Backtrack
-
-  val cn = cs flatMap { c2 => prop(c, c2)(fail) }
-  cstore += c
-  cn foreach register
-  false
-}
-
-def register(c: Constraint): Unit = {
-  if (cstore.contains(c)) return
-  if (conflict(cstore,c)) throw Backtrack
-}
+  def register(c: Constraint): Unit = {
+    if (cstore.contains(c)) return
+    if (conflict(cstore,c)) throw Backtrack
+  }
 }
 
 trait Engine extends Base {
-// execution (depth-first)
-def run[T](f: Exp[T] => Rel): Seq[String] = {
-  def call(e: () => Rel)(k: Cont): Unit = {
-    val cstore1 = cstore
-    val dvars1 = dvars
-    try {
-      e().exec(call)(k)
-    } catch {
-      case Backtrack => // OK
-    } finally {
-      cstore = cstore1
+  // execution (depth-first)
+  def run[T](f: Exp[T] => Rel): Seq[String] = {
+    def call(e: () => Rel)(k: Cont): Unit = {
+      val cstore1 = cstore
+      val dvars1 = dvars
+      try {
+        e().exec(call)(k)
+      } catch {
+        case Backtrack => // OK
+      } finally {
+        cstore = cstore1
       dvars = dvars1
+      }
     }
+    val q = fresh[T]
+    val res = mutable.ListBuffer[String]()
+    call(() => f(q)) { () =>
+      res += extractStr(q)
+    }
+    res.toList
   }
-  val q = fresh[T]
-  val res = mutable.ListBuffer[String]()
-  call(() => f(q)) { () =>
-    res += extractStr(q)
-  }
-  res.toList
-}
 
-def runN[T](max: Int)(f: Exp[T] => Rel): Seq[String] = {
-  def call(e: () => Rel)(k: Cont): Unit = {
-    val cstore1 = cstore
-    val dvars1 = dvars
-    try {
-      e().exec(call)(k)
-    } catch {
-      case Backtrack => // OK
-    } finally {
-      cstore = cstore1
-      dvars = dvars1
+  def runN[T](max: Int)(f: Exp[T] => Rel): Seq[String] = {
+    def call(e: () => Rel)(k: Cont): Unit = {
+      val cstore1 = cstore
+      val dvars1 = dvars
+      try {
+        e().exec(call)(k)
+      } catch {
+        case Backtrack => // OK
+      } finally {
+        cstore = cstore1
+        dvars = dvars1
+      }
     }
-  }
-  val q = fresh[T]
-  val res = mutable.ListBuffer[String]()
-  val Done = new Exception
-  try {
-  call(() => f(q)) { () =>
-    res += extractStr(q)
+    val q = fresh[T]
+    val res = mutable.ListBuffer[String]()
+    val Done = new Exception
+    try {
+      call(() => f(q)) { () =>
+        res += extractStr(q)
     if (res.length>=max) throw Done
+      }
+    } catch { case Done => }
+    res.toList
   }
-  } catch { case Done => }
-  res.toList
-}
 
   // def extractStr(x: Exp[Any]): String
 
